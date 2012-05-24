9114354ccff883a5090667c8736abece804538d8
Andrew Emelianenko <emelyanenko.web@gmail.com>
2012-05-24 18:46:14 +0300

    fix view

--- views/message/basic.php
+++ views/message/basic.php
@@ -1,7 +1,7 @@
 <ul id="messages">
-	<?php foreach ($messages as $message) { ?>
+	<?php foreach ($messages as $message): ?>
 		<li class="<?php echo $message->type ?>">
 			<p><?php echo $message->text ?></p>
 		</li>
-	<?php } ?>
+	<?php endforeach; ?>
 </ul>
